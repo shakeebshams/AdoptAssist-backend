268049058ec38bf73ae6fbf0ae274cef4c4da1e7
handerson44 <handerson44@gatech.edu>
2021-11-29 18:50:16 -0500

    More changes

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,6 @@
+// const _server = express()
+//import _server from '../server.js'
+
 /*----------------------------------------------------*/
 /* Quote Loop
 ------------------------------------------------------ */
@@ -48,27 +51,38 @@ $(document).ready(function() {
     });
 });
 
+// jQuery(document).ready(function($) {
+
+//     $('.button').on('click',function (e) {
+//         e.preventDefault();
+//         console.log("button click")
+//         // _server.post('/', function(req, res) {
+//         // console.log(__dirname)
+//         // res.sendFile(path.join(__dirname, '/findAPet.html'))
+//         // });
+//     });
+// });
+
 
 /*----------------------------------------------------*/
 /* Smooth Scrolling
 ------------------------------------------------------ */
 
-jQuery(document).ready(function($) {
+// jQuery(document).ready(function($) {
 
-   $('.smoothscroll').on('click',function (e) {
-	    e.preventDefault();
+//    $('.smoothscroll').on('click',function (e) {
+// 	    e.preventDefault();
 
-	    var target = this.hash,
-	    $target = $(target);
+// 	    var target = this.hash,
+// 	    $target = $(target);
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 800, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
-  
-});
+// 	    $('html, body').stop().animate({
+// 	        'scrollTop': $target.offset().top
+// 	    }, 800, 'swing', function () {
+// 	        window.location.hash = target;
+// 	    });
+// 	});
+// });
 
 
 TweenMax.staggerFrom(".heading", 0.8, {opacity: 0, y: 20, delay: 0.2}, 0.4);
--- server.js
+++ server.js
@@ -4,23 +4,44 @@ import { fileURLToPath } from 'url'
 import axios from 'axios'
 import ejs from 'ejs'
 import dotenv from 'dotenv'
+import { truncateSync } from 'fs'
 dotenv.config()
 
 const port = process.env.PORT || 3000
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const _server = express()
 _server.set('view engine', 'ejs')
+//const findAPet = require('/frontend/findAPet')
+
+function isEmpty(obj) {
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
+    }
+    return JSON.stringify(obj) === JSON.stringify({});
+}
 
 _server.use(express.urlencoded({extended: true}))
 _server.use(express.json())
+
 //_server.use(express.static(path.join(__dirname ,'/assets')))
 _server.use('/frontend', express.static(path.join(__dirname, '/frontend')))
-
+// _server.use('/frontend/findAPet', express.static(path.join(__dirname, '/frontend/findAPet')))
 
 
 _server.get('/', function(req, res) {
     console.log(__dirname)
     res.sendFile(path.join(__dirname, '/frontend/index.html'))
+})
+
+
+_server.post('/', function(req, res) {
+    console.log(__dirname)
+    var newFile = '/frontend/findAPet.html'
+    if (isEmpty(req.body)) {
+        newFile = '/frontend/login.html'
+    }
+    res.sendFile(path.join(__dirname, newFile))
 })
 
 _server.get('/frontend/styles.css', function(req, res) {
@@ -31,3 +52,4 @@ _server.get('/frontend/styles.css', function(req, res) {
 _server.listen(port, function() {
     console.log("Web server listening on port: " + port)
 })
+
